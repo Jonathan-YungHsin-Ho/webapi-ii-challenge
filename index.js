a5712403746bab7d3ed5958800ff6dd6e78d69fe
Jonathan Ho <johnnyho82@gmail.com>
2019-10-03 16:53:39 -0400

    Prepare for heroku deployment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 
@@ -12,7 +14,7 @@ server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => res.send('API up and running!'));
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () =>
   console.log(`\n*** Server Running on Port ${port} ***\n`),
 );
